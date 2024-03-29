16ec5bce7608bbfdc6eb1f99feaf7bf21f20ad96
shu-wenjie-cc <1841193728@qq.com>
2024-03-29 15:12:53 +0800

    操作符学习

---
+++ one.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	printf("%d\n",7/5);
+//	printf("%f\n",12.0/5);
+//	printf("%d\n",7%5);
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int a = -7;
+//	int b = a << 1;
+//	printf("%d", a);
+//	printf("%d", b);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 7;
+//	int d = -7;
+//	int b = a >> 1;
+//	int c = d >> 1;
+//	printf("%d %d", b,c);
+//	return 0;
+//}
+//& | ^
+//int main()
+//{
+//	int a = 3;
+//	int b = -5;
+//	int c = a | b;
+//	printf("%d\n",c);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 5;
+//	int b = 3;
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("%d %d", a, b);
+//	return 0;
+//}
+int main()
+{
+	int a = 0;
+	printf("%p", &a);
+	return 0;
+}
